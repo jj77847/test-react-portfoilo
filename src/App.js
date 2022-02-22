210e8e6824268a930c622700bc4e3ff78d9dffe2
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-22 15:48:35 +0000

    files and folders

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Header } from "./components/Header";
+import "./App.css";
+import { Projects } from "./components/Projects";
+
+const projectsFromApi = [
+  {
+    title: "Project 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, eligendi repudiandae? Cum modi eos earum minus, beatae, obcaecati officiis facilis, et neque reiciendis officia consequatur quam? Sint, aspernatur? Quasi, commodi!",
+    link: "https://www.google.co.uk/",
+  },
+  {
+    title: "Project 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, eligendi repudiandae? Cum modi eos earum minus, beatae, obcaecati officiis facilis, et neque reiciendis officia consequatur quam? Sint, aspernatur? Quasi, commodi!",
+    link: "https://www.google.co.uk/",
+  },
+  {
+    title: "Project 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, eligendi repudiandae? Cum modi eos earum minus, beatae, obcaecati officiis facilis, et neque reiciendis officia consequatur quam? Sint, aspernatur? Quasi, commodi!",
+    link: "https://www.google.co.uk/",
+  },
+  {
+    title: "Project 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, eligendi repudiandae? Cum modi eos earum minus, beatae, obcaecati officiis facilis, et neque reiciendis officia consequatur quam? Sint, aspernatur? Quasi, commodi!",
+    link: "https://www.google.co.uk/",
+  },
+];
+
+const App = (props) => {
+  return (
+    <div>
+      <Header title="Jonathan Jefferies" subTitle="Full Stack Developer" />
+      <main>
+        <Projects projects={projectsFromApi} />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+  <App title="Mr." fullName="Jonathan Jefferies" />,
+  document.getElementById("root")
+);
